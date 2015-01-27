35c631cab212961e8d350f00749533aaaae89d57
Tijndagamer <tijndagamer25@gmail.com>
2015-01-27 20:17:34 +0100

    Ultrasonic - Update 1, Initial commit
    - Added Ultrasonic\ , which will contain all the programs I wrote for my
    HC-SR04 Ultrasonic range finder
    - Added UltrasonicRangeFinder.py

---
+++ Ultrasonic/UltrasonicRangeFinder.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+
+#Global Variables
+ECHO = 24
+TRIG = 25
+
+#Setup the GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(TRIG, GPIO.LOW)
+
+GPIO.setup(ECHO, GPIO.IN)
+
+#Returns the range in CM or M
+#If returnFormat is True, it will return in CM
+#If returnFormat is False, it will return in M
+def MeasureRange(returnFormat):
+    #First sleep to make sure that we aren't overloading the HC-SR04 
+    time.sleep(0.1)
+
+    #Send the trigger signal
+    GPIO.output(TRIG, GPIO.HIGH)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, GPIO.LOW)
+ 
+    #Listen to the ECHO pin
+    while GPIO.input(ECHO) == 0:
+        pass
+    start = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pass
+    stop = time.time()
+
+    #Check what to return
+    if (returnFormat == True):
+        return (stop - start) * 17000
+    if (returnFormat == False):
+        return (stop - start) * 170
+
+while True:
+    print(str(MeasureRange(True)) + " cm")
