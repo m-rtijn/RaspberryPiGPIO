18699d00b66fcdb019fd8b83edf6fd058b71b800
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-01-28 10:26:07 +0100

    control script PEP-8
    mostly fixing whitespace

--- LEDs/Control.py
+++ LEDs/Control.py
@@ -1,42 +1,52 @@
-#Made by Tijndagamer. (C) 2014
+"""Created by MrTijn/Tijndagamer
+Control the GPIO
+Copyright 2014
+"""
 
-#Importing libraries
+# Importing libraries
 import sys
 import time
 import RPi.GPIO as GPIO
 
-#Setup the GPIO
+# Setup the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
 
 def SetupOutput(Pin): #Setups a pin as an output pin
 	GPIO.setup(Pin, GPIO.OUT)
 	print("The pin " + str(Pin) + " is now an output pin")
 
+
 #def SetupInput(Pin): #Setups a pin as an input pin
 #	GPIO.setup(Pin, GPIO.IN)
 #	print("The pin " + str(Pin) + " is now an input pin")
 
+
 def SetHigh(Pin): #Sets a pin high
 	SetupOutput(Pin)
 	GPIO.output(Pin, GPIO.HIGH)
+
+
 def SetLow(Pin):
 	SetupOutput(Pin)
 	GPIO.output(Pin, GPIO.LOW)
 
+
 def main():
 	print("Use: sudo python Control.py [pinnumber] [state]")
-	#First check if there are enough arguments
-	if len(sys.argv) == 3:
+	# First check if there are enough arguments
+
+    if len(sys.argv) == 3:
 		if sys.argv[2] == "HIGH":
-			#First convert argv[1] to an int
+			# First convert argv[1] to an int
 			param = int(sys.argv[1])
-			#Call SetHigh to set the pin high
+			# Call SetHigh to set the pin high
 			SetHigh(param)
 		if sys.argv[2] == "LOW":
-			#First convert argv[1] to an int
+			# First convert argv[1] to an int
 			param = int(sys.argv[1])
-			#Call SetLow to set the pin low
+			# Call SetLow to set the pin low
 			SetLow(param)
 		if sys.argv[1] == "cleanup":
 			print("Cleaning up this mess")
@@ -47,8 +57,10 @@ def main():
 			sys.exit()
 	else:
 		print("Error, reached else statement. exiting script [0]")
-		GPIO.cleanup()
+
+        GPIO.cleanup()
 		sys.exit()
+
 
 main()
 GPIO.cleanup()
