3596598d9b77f36de5e038f5928ab04c9ac2c72d
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-01-28 10:25:26 +0100

    calculator.py PEP-8
    mostly fixing whitespace

--- LEDNumberDisplay/Calculator.py
+++ LEDNumberDisplay/Calculator.py
@@ -1,15 +1,24 @@
-#Import libraries
+"""Created by MrTijn/Tijndagamer
+Copyright 2015
+"""
+
+# Import libraries
 import os
 import sys
 
+
 def PrintOutput(output):
-#	print(output)
+    """Prints something specified by the user"""
+
 	outputList = map(int, str(output))
-	for digit in outputList:
-#		print(digit)
+	
+    for digit in outputList:
 		os.system("sudo python NumberPrinter.py " + str(digit) + " 1")
 
+
 def ArgumentHandler():
+    """Handles the arguments passed to the script"""
+
 	if sys.argv[2] == "+":
 		PrintOutput(int(sys.argv[1]) + int(sys.argv[3]))
 	if sys.argv[2] == "-":
@@ -20,4 +29,5 @@ def ArgumentHandler():
 		PrintOutput(int(sys.argv[1]) / int(sys.argv[3]))
 	if sys.argv[1] == "help":
 		print("usage: sudo python Calculator.py [a] [operator] [b]")
+
 ArgumentHandler()
