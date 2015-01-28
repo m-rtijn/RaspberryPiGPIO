26027800855d6f39a8ad847a7c3e91ecdc034741
Tijndagamer <tijndagamer25@gmail.com>
2015-01-28 08:44:19 +0100

    Ultrasonic - Update 1.1, Code now follows the PEP8 guidelines

--- Ultrasonic/UltrasonicRangeFinder.py
+++ Ultrasonic/UltrasonicRangeFinder.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-#Global Variables
+# Global Variables
 ECHO = 24
 TRIG = 25
 
-#Setup the GPIO
+# Setup the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -14,19 +14,19 @@ GPIO.setup(TRIG, GPIO.LOW)
 
 GPIO.setup(ECHO, GPIO.IN)
 
-#Returns the range in CM or M
-#If returnFormat is True, it will return in CM
-#If returnFormat is False, it will return in M
+# Returns the range in CM or M
+# If returnFormat is True, it will return in CM
+# If returnFormat is False, it will return in M
 def MeasureRange(returnFormat):
-    #First sleep to make sure that we aren't overloading the HC-SR04 
+    # First sleep to make sure that we aren't overloading the HC-SR04
     time.sleep(0.1)
 
-    #Send the trigger signal
+    # Send the trigger signal
     GPIO.output(TRIG, GPIO.HIGH)
     time.sleep(0.00001)
     GPIO.output(TRIG, GPIO.LOW)
- 
-    #Listen to the ECHO pin
+
+    # Listen to the ECHO pin
     while GPIO.input(ECHO) == 0:
         pass
     start = time.time()
@@ -35,10 +35,10 @@ def MeasureRange(returnFormat):
         pass
     stop = time.time()
 
-    #Check what to return
-    if (returnFormat == True):
+    # Check what to return
+    if (returnFormat is True):
         return (stop - start) * 17000
-    if (returnFormat == False):
+    if (returnFormat is False):
         return (stop - start) * 170
 
 while True:
