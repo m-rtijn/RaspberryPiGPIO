08ca1ab4e6f9951301c8bb83a359309e60dc74a1
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-01-28 09:40:46 +0100

    fixed cds-reader
    PEP-8 and PEP-20 are now being followed

--- CdS/CdSReader.py
+++ CdS/CdSReader.py
@@ -1,22 +1,28 @@
-#Most of this code is copied from this tutorial: https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi?view=all
+"""Most of this code is copied from this tutorial:
+https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi?view=all
+"""
 
-#Importing the libraries
+# Importing the libraries
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+
 def ReadCdSCel(CdSCelPin):
-	reading = 0
-	GPIO.setup(CdSCelPin, GPIO.OUT)
-	GPIO.output(CdSCelPin, GPIO.LOW)
-	time.sleep(0.1)
+    reading = 0
+    GPIO.setup(CdSCelPin, GPIO.OUT)
+    GPIO.output(CdSCelPin, GPIO.LOW)
+    time.sleep(0.1)
 
 	GPIO.setup(CdSCelPin, GPIO.IN)
-	while (GPIO.input(CdSCelPin) == GPIO.LOW):
+
+    while (GPIO.input(CdSCelPin) == GPIO.LOW):
 		reading += 1
-	return reading
+
+    return reading
+
 
 while True:
-	print ReadCdSCel(18)
+    print ReadCdSCel(18)
