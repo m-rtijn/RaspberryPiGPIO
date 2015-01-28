f80031dd808ce167a652a022404d9ef5108e4d3b
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-01-28 10:25:42 +0100

    blink script PEP8
    mostly fixing whitespace

--- LEDs/blink.py
+++ LEDs/blink.py
@@ -1,16 +1,22 @@
+"""Created by MrTijn/Tijndagamer
+Blinks an LED
+"""
+
 import RPi.GPIO as GPIO
 import time
 import sys
 
-#Setup the GPIO
+# Setup the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
 
 def main():
 	if sys.argv[1] == "help":
 		print("sudo python blink.py <pinnumber> <time in seconds>")
 		sys.exit()
-	GPIO.setup(int(sys.argv[1]), GPIO.OUT)
+	
+    GPIO.setup(int(sys.argv[1]), GPIO.OUT)
 	GPIO.output(int(sys.argv[1]), GPIO.HIGH)
 	time.sleep(int(sys.argv[2]))
 	GPIO.output(int(sys.argv[1]), GPIO.LOW)
