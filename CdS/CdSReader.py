1d81edb4de509bc3aa4f990673163a5b3c007adb
Tijndagamer <tijndagamer25@gmail.com>
2015-01-28 09:04:49 +0100

    CdS - Update 1.2, Fixed PEP8 errors

--- CdS/CdSReader.py
+++ CdS/CdSReader.py
@@ -1,24 +1,25 @@
-#Most of this code is copied from this tutorial: https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi?view=all
+# Most of this code is copied from this tutorial:
+# https://learn.adafruit.com/basic-resistor-sensor-reading-on-raspberry-pi?view=all
 
-#Importing the libraries
+# Importing the libraries
 import RPi.GPIO as GPIO
 import time
 
-#Setup the GPIO
+# Setup the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-#Reads the CdS cel and returns the reading
+# Reads the CdS cel and returns the reading
 def ReadCdSCel(CdSCelPin):
-	reading = 0
-	GPIO.setup(CdSCelPin, GPIO.OUT)
-	GPIO.output(CdSCelPin, GPIO.LOW)
-	time.sleep(0.1)
+    reading = 0
+    GPIO.setup(CdSCelPin, GPIO.OUT)
+    GPIO.output(CdSCelPin, GPIO.LOW)
+    time.sleep(0.1)
 
-	GPIO.setup(CdSCelPin, GPIO.IN)
-	while (GPIO.input(CdSCelPin) == GPIO.LOW):
-		reading += 1
-	return reading
+    GPIO.setup(CdSCelPin, GPIO.IN)
+    while (GPIO.input(CdSCelPin) == GPIO.LOW):
+        reading += 1
+    return reading
 
 while True:
-	print ReadCdSCel(18)
+    print ReadCdSCel(18)
